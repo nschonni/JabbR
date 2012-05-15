e5326efee3ef3a3f7fbc7e6c19b99002d48a3805
nschonni <nschonni@gmail.com>
2012-05-14 22:36:39 -0400

    /who [username] pulls down basic information from gravatar.com #76

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -454,6 +454,13 @@
             ui.addMessage('Note: ' + userInfo.Note, 'list-item');
         }
 
+      $.getJSON('http://www.gravatar.com/' + userInfo.Hash + '.json?callback=?', function (profile) {
+        ui.addMessage('Gravatar', 'list-header');
+        ui.addMessage('<img src="' + profile.entry[0].thumbnailUrl + '" />', 'list-item');
+        ui.addMessage('Profile Link: <a href="' + profile.entry[0].profileUrl + '">' + profile.entry[0].displayName + '</a>', 'list-item');
+        ui.addMessage('Name: ' + profile.entry[0].name.formatted, 'list-item');
+      });     
+
         chat.showUsersOwnedRoomList(userInfo.Name, userInfo.OwnedRooms);
     };
 
